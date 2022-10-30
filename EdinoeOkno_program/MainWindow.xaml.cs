dc2fdb1a3a193bb68006233e36d5c56f577cbc53
YaroslavVotincev <ya.lisicheg@yandex.ru>
2022-10-30 21:58:31 +0400

    пересоздал wpf

--- EdinoeOkno_program/MainWindow.xaml.cs
+++ EdinoeOkno_program/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace EdinoeOkno_program
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -24,6 +24,5 @@ namespace EdinoeOkno_program
         {
             InitializeComponent();
         }
-
     }
 }
